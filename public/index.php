36a79c5642d8c22300d7833a59866cdba27c01b6
Said <babaiev.info@gmail.com>
2020-06-22 23:47:52 +0300

    Сделал autoload для подключение классов

--- public/index.php
+++ public/index.php
@@ -2,12 +2,29 @@
 
 echo $query = rtrim($_SERVER['QUERY_STRING'], '/');
 
+define('WWW', __DIR__);
+define('CORE', dirname(__DIR__) . '/vendor/core');
+define('ROOT', dirname(__DIR__));
+define('APP', dirname(__DIR__) . '/app');
+
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
-require '../app/controllers/Posts.php';
-require '../app/controllers/PostsNew.php';
 
+//require '../app/controllers/Main.php';
+//require '../app/controllers/Posts.php';
+//require '../app/controllers/PostsNew.php';
 
+spl_autoload_register(function ( $class ) {
+   $file = APP . "/controllers/$class.php";
+   if(is_file($file)) {
+       require_once $file;
+   }
+});
+
+// custom routs
+Router::add('^pages/?(?P<action>[a-z-]+)?$', ['controller' => 'Posts']);
+
+// default routs
 Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
 Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
 
