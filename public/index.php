2e4db0181e13da126ae5792c74b74bc0bc4ffa09
Said <babaiev.info@gmail.com>
2020-06-22 23:31:35 +0300

    Расширил класс Router. Добавил несколько контроллеров

--- public/index.php
+++ public/index.php
@@ -4,16 +4,13 @@ echo $query = rtrim($_SERVER['QUERY_STRING'], '/');
 
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
+require '../app/controllers/Posts.php';
+require '../app/controllers/PostsNew.php';
 
-Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
-Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
-Router::add('', ['controller' => 'Main', 'action' => 'index']);
 
+Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
 
 debug(Router::getRoutes());
 
-if(Router::matchRoute($query)) {
-    debug(Router::getRoute());
-} else {
-    echo '404';
-}
+Router::dispatch($query);
